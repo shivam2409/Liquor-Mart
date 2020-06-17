03d9b2976333c8bef621b1b4e33fca07e9e0dfc4
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2020-06-17 11:20:29 -0400

    MongoDB connected

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  //Get token from Header
+
+  const token = req.header('x-auth-token');
+
+  //check if token is there
+  if (!token) {
+    return res.status(401).json({ msg: 'No token,Authorization denied' });
+  }
+  //Verify token
+
+  try {
+    const decoded = jwt.verify(token, config, config.get('jwtSecret'));
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: 'Token is not velid' });
+  }
+};
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/database');
 
 const app = express();
+
+//Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => console.log(`Server Runnong on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
